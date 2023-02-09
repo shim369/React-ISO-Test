54e25e6335d61cdedeb27872cce33e6309780eef
ohtani shimpei <ohtani.shimpei@gmail.com>
2023-02-09 18:46:21 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useInView } from 'react-intersection-observer';
 import './App.css';
+import 'animate.css';
 
 function App() {
+	const { ref, inView } = useInView({
+		rootMargin: '-100px',
+		triggerOnce: false,
+	});
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+		<div style={{ height: '800px', backgroundColor: 'blue' }}></div>
+		<div ref={ref} style={{ height: '800px',backgroundColor: 'yellow' }}>
+			{inView && (
+				<div className="text animate__animated animate__fadeInUp">text</div>
+			)}
+		</div>
     </div>
   );
 }
